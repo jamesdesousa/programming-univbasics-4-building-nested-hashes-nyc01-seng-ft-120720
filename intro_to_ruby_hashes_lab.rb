ef3bdc8fa1deaf26d2f9ef25834d9778241bcab8
James DeSousa <james.desousa45@gmail.com>
2020-11-25 00:34:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,7 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  newh = {:railroads => {{:pieces => "4"}, {:rent_in_dollars => {}}, {:names => {} }}
+  newh = {:railroads => {{:pieces => "4"}, {:rent_in_dollars => {}}, {:names => {}} }}
 
 end
 
